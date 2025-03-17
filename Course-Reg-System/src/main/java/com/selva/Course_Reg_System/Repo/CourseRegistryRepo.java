14e3b60cec510467cbce2b5cfd94b726af7fe5b8
selvapraveenv <selvapraveen0405@gmail.com>
2025-03-17 21:04:56 +0530

    Delete Course-Reg-System directory

--- Course-Reg-System/src/main/java/com/selva/Course_Reg_System/Repo/CourseRegistryRepo.java
+++
@@ -1,7 +0,0 @@
-package com.selva.Course_Reg_System.Repo;
-
-import com.selva.Course_Reg_System.Model.CourseRegistry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRegistryRepo extends JpaRepository<CourseRegistry,Integer> {
-}
